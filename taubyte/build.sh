b390d6598d45a3d9f53b8caa59effb5fa547bffe
taubyte-test <104384673+taubyte-test@users.noreply.github.com>
2022-12-28 13:16:53 -0600

    Update build.sh

--- taubyte/build.sh
+++ taubyte/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-pwd
+hugo new site website && cd website && git init
+git submodule add --depth=1 https://github.com/adityatelange/hugo-PaperMod.git themes/PaperMod && git submodule update --init --recursive
+echo "theme: \"PaperMod\"" >> config.yml && rm config.toml
+hugo 
+mv /src/website/public/* /out
 
-ls /src 
-
-git submodule init && git submodule update && hugo && mv public/* /out
 
 exit $?
